3fecbbe2638a08f224fc8004a61f0564edfcec84
Mahmoud Mirghafari <mahmoud.mirghafari@gmail.com>
2022-05-28 12:08:20 +0430

    work on app infrastructure
    -move MainActivity to ui package
    -update MainActivity ui
    -update theme and style
    -update colors
    -add navigation menu and it icons
    -add app navigation

---
+++ app/src/main/java/com/sky/social/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.sky.social.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.sky.social.R
+import dagger.hilt.android.AndroidEntryPoint
+
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initBottomNavigationBar()
+    }
+
+    private fun initBottomNavigationBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+}
