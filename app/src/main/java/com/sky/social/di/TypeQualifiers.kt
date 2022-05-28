8b3aaf5baa2933da3f7f824b3524e1bbe4139cfa
Mahmoud Mirghafari <mahmoud.mirghafari@gmail.com>
2022-05-28 12:12:14 +0430

    work on app infrastructure
    -add CoroutinesModule

---
+++ app/src/main/java/com/sky/social/di/TypeQualifiers.kt
@@ -0,0 +1,16 @@
+package com.sky.social.di
+
+import javax.inject.Qualifier
+
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainDispatcher
