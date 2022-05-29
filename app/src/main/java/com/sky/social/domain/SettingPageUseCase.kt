fdc090559edfabfd1d904090b5c34b0cfa9c388a
Mahmoud Mirghafari <mahmoud.mirghafari@gmail.com>
2022-05-29 11:47:09 +0430

    refactor: improve the SettingPageUseCase and impl

--- app/src/main/java/com/sky/social/domain/SettingPageUseCase.kt
+++ app/src/main/java/com/sky/social/domain/SettingPageUseCase.kt
@@ -1,11 +1,12 @@
 package com.sky.social.domain
 
+import com.sky.social.data.ResultState
 import com.sky.social.data.VideoData
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharedFlow
 
 
 interface SettingPageUseCase {
-    fun getVideos(): Flow<SharedFlow<Result<List<VideoData>>>>
+    fun getVideos(): Flow<ResultState<List<VideoData>>>
     fun updateVideoLikeAndViewCounter(videoId: String?, likes: Int, views: Int): Flow<Result<Nothing>>
 }
