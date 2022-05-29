aa0ba2e38154b4a2d17157380c487c04bc94b653
Mahmoud Mirghafari <mahmoud.mirghafari@gmail.com>
2022-05-29 11:49:58 +0430

    feat: add SettingFragment class

---
+++ app/src/main/java/com/sky/social/ui/settingTab/SettingFragment.kt
@@ -0,0 +1,12 @@
+package com.sky.social.ui.settingTab
+
+import androidx.fragment.app.Fragment
+import com.sky.social.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingFragment : Fragment() {
+
+    private lateinit var binding: FragmentSettingBinding
+
+}
